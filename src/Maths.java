35cb9b8fff99e76d83ca4a3796c541c0955f0c98
Wojciech Kos <obohp@wp.pl>
2020-06-14 10:43:05 +0200

    code reformatted

--- src/Maths.java
+++ src/Maths.java
@@ -1,8 +1,17 @@
 public class Maths {
-    public boolean isEven(int liczba){ return liczba%2 == 0; }
-    public boolean isOdd(int liczba){ return liczba%2 != 0; }
-    public double circleField(double radius){
+    public boolean isEven(int liczba) {
+        return liczba % 2 == 0;
+    }
+
+    public boolean isOdd(int liczba) {
+        return liczba % 2 != 0;
+    }
+
+    public double circleField(double radius) {
         return Math.PI * radius * radius;
     }
-    public int power(int number){ return number * number; }
+
+    public int power(int number) {
+        return number * number;
+    }
 }
